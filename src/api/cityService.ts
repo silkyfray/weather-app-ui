477caf9a982bd31630a77c78f79dd14e5bb0e876
Tony Nikolov <tony.nikolov@ivendi.com>
2019-02-19 08:36:01 +0000

    generalise input name as it may contain country too

--- src/api/cityService.ts
+++ src/api/cityService.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import { OwmWeatherResponse } from "./types/OwmResponse";
 
-export async function FetchCity24HourForecast(cityName: string) {
-  console.log(cityName);
+export async function FetchCity24HourForecast(searchText: string) {
   const res = await axios.get<OwmWeatherResponse>(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=8&units=metric&apikey=886705b4c1182eb1c69f28eb8c520e20`
+    `http://api.openweathermap.org/data/2.5/forecast?q=${searchText}&cnt=8&units=metric&apikey=886705b4c1182eb1c69f28eb8c520e20`
   );
 
   return res.data;
